dc5dd1c07bd9f43c9713bd8103a35a4efb49c0d9
Terrence Wong <wongte@oocl.com>
2020-04-23 20:45:53 +0800

    Initial Commit

---
+++ src/main/java/com/intelligentcat/parkwithyoubackend/ParkWithYouBackendApplication.java
@@ -0,0 +1,13 @@
+package com.intelligentcat.parkwithyoubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkWithYouBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkWithYouBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intelligentcat/parkwithyoubackend/ParkWithYouBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelligentcat.parkwithyoubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkWithYouBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
