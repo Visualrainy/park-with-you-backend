e231fee435feb3495886f7ae12ded19898c3ae73
Peng Chen <pchen@thoughtworks.com>
2020-04-27 14:14:37 +0800

    Add application-prod.yml file

--- src/main/java/com/intelligentcat/parkwithyoubackend/controller/TestController.java
+++ src/main/java/com/intelligentcat/parkwithyoubackend/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.intelligentcat.parkwithyoubackend.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class TestController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping
     public String test() {
-        return "Success";
+        return "Success" + serverPort;
     }
 }
